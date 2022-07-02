f602b9a9d2d46ecc498c76f59d1e3da6637c626e
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-07-02 15:45:37 -0700

    GREAYT_MACRO_PYTHON3_V1.py
    An archive of the first version of the Greayt macro for Python 3.

---
+++ P/Python/OldVersions/3/GREAYT_MACRO_PYTHON3_V1.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env/python
+# Start of script
+# Graeyt macro : Python edition
+print("Graeyt macro options:\n")
+print("Grey -> Gray (ID: 1)")
+print("Gray -> Grey (ID: 2)")
+print("GREY -> GRAY (ID: 3)")
+print("GRAY -> GREY (ID: 4)")
+print("Grey -> Græy (ID: 5)")
+print("Gray -> Græy (ID: 6)")
+print("GREY -> GRÆY (ID: 7)")
+print("GRAY -> GRÆY (ID: 8)")
+print("Græy -> Grey (ID: 9)")
+print("Græy -> Gray (ID: 10)")
+print("GRÆY -> GREY (ID: 11)")
+print("GRÆY -> GRAY (ID: 12)")
+input1 = int(input("Enter an ID: "))
+if (input1 == "1"):
+  for x in range(1, 999999999999):
+    str(" Grey ") -> (" Gray " )
+    break
+elif (input1 == "2"):
+  for x in range(1, 999999999999):
+    str(" Gray ") -> (" Grey " )
+    break
+elif (input1 == "3"):
+  for x in range(1, 999999999999):
+    str(" GREY ") -> (" GRAY " )
+    break
+elif (input1 == "4"):
+  for x in range(1, 999999999999):
+    str(" GRAY ") -> (" GREY " )
+    break
+elif (input1 == "5"):
+  for x in range(1, 999999999999):
+    str(" Grey ") -> (" Græy " )
+    break
+elif (input1 == "6"):
+  for x in range(1, 999999999999):
+    str(" Gray ") -> (" Græy " )
+    break
+elif (input1 == "7"):
+  for x in range(1, 999999999999):
+    str(" GREY ") -> (" GRÆY " )
+    break
+elif (input1 == "8"):
+  for x in range(1, 999999999999):
+    str(" GRAY ") -> (" GRÆY " )
+    break
+elif (input1 == "9"):
+  for x in range(1, 999999999999):
+    str(" Græy ") -> (" Grey " )
+    break
+elif (input1 == "10"):
+  for x in range(1, 999999999999):
+    str(" Græy ") -> (" Gray " )
+    break
+elif (input1 == "11"):
+  for x in range(1, 999999999999):
+    str(" GRÆY ") -> (" GREY " )
+    break
+elif (input1 == "12"):
+  for x in range(1, 999999999999):
+    str(" GRÆY ") -> (" GRAY " )
+    break
+# Error handling goes here
+"""
+try
+except
+catch
+"""
+noMore = input("Press [ENTER] key to exit")
+print("The program is now closing...")
+break
+# File info
+# File type: Python source file (*.py)
+# File version: 1 (2022, Saturday, July 2nd at 3:45 pm PST)
+# Line count (including blank lines and compiler line): 80
+# End of script
